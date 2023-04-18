da1778532cf93cb2feced333cae6aeda1aab1616
aramis-matos <aramis.matos1@gmail.com>
2023-04-18 18:55:45 -0400

    guh

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -x GTK_IM_MODULE 'fcitx'
+set -x QT_IM_MODULE 'fcitx'
+set -x SDL_IM_MODULE 'fcitx'
+set -x XMODIFIERES '@im=fcitx'
+#set -x PATH $PATH "~/.cabal/bin/"
+#set -x PATH $PATH "~/.ghcup/bin/"
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/ccyanide/.ghcup/bin # ghcup-env
+set fish_greeting
+set -x EDITOR 'nvim'
+alias emacs="emacsclient -c -a 'emacs'"
