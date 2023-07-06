489ffd3142f5b981177fae340464201e8d167c9a
aramis-matos <70601929+aramis-matos@users.noreply.github.com>
2023-07-06 10:39:26 -0400

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,10 @@ set -x SDL_IM_MODULE 'fcitx'
 set -x XMODIFIERES '@im=fcitx'
 #set -x PATH $PATH "~/.cabal/bin/"
 #set -x PATH $PATH "~/.ghcup/bin/"
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/ccyanide/.ghcup/bin # ghcup-env
+set -x PATH $PATH "/home/ccyanide/Downloads/waifu2x-ncnn-vulkan-20220728-ubuntu/"
+#set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/ccyanide/.ghcup/bin # ghcup-env
 set fish_greeting
 set -x EDITOR 'nvim'
 alias emacs="emacsclient -c -a 'emacs'"
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/ccyanide/.ghcup/bin # ghcup-env
